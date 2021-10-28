13b115c3ab2136889c47066227be5c5ccd595f1f
shindeanant <shindeanant1997@gmail.com>
2021-10-28 23:47:10 +0530

    Added: Shuffling the Cards.

--- src/main/java/com/bridgelabz/deckofcards/DeckOfCardsGam.java
+++ src/main/java/com/bridgelabz/deckofcards/DeckOfCardsGam.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * Purpose  - There should be minimum 2 and maximum 4 players in the Game
- * @author  - Anant shinde
+ * Purpose - There should be minimum 2 and maximum 4 players in the Game
+ * 
+ * @author - Anant shinde
  * @version - 16.0
- * @since   - 2021-10-01
+ * @since - 2021-10-01
  */
 public class DeckOfCardsGam {
 	public static final Scanner scanner = new Scanner(System.in);
@@ -16,7 +17,6 @@ public class DeckOfCardsGam {
 	/*
 	 * Simply created a Welcome Message Method without any return type.
 	 */
-
 	public void welcome() {
 		System.out.println("Welcome to the gaming world of Deck of Cards");
 	}
@@ -27,12 +27,11 @@ public class DeckOfCardsGam {
 	 * this we are printing the size of card. In this method we are also printing
 	 * all the possible combination of card.
 	 */
-
 	public void deckOfCards() {
 		String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
 		String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace" };
 		int numOfCards = suits.length * ranks.length;
-		System.out.println("\nNumber of cards in the deck:" + numOfCards);
+		System.out.println("\nNumber of cards in the deck is : " + numOfCards);
 		for (int i = 0; i < ranks.length; i++) { // First we have to itterate for loop for ranks starting from index 0.
 			for (int j = 0; j < suits.length; j++) { // Now we have to ittreate the suits for all the indexes of ranks
 				cardsDeck.add(ranks[i] + "----->" + suits[j]);
@@ -46,7 +45,6 @@ public class DeckOfCardsGam {
 	 * for-each loop to print all the element of ArrayList. We are storing all the
 	 * elements of cardsDeck arraylist in element.
 	 */
-
 	public static void toDisplay(ArrayList<String> cardsDeck) {
 		System.out.println("\nCards in Deck:");
 		for (String element : cardsDeck) {
@@ -60,16 +58,45 @@ public class DeckOfCardsGam {
 	 * we have use if condition to define no of players. If no of player is not
 	 * within the range then it will again call this method.
 	 */
-
 	public void noOfPlayers() {
 		System.out.print("\nEnter number of players minimum 2 , maximum 4 : ");
 		int player = scanner.nextInt();
 		if (player >= 2 && player <= 4) {
 			System.out.println("\n" + player + " players will play the game");
+			sequenceOfPlay(player);
 		} else {
 			System.out.println("Please enter number of players in the Range");
-			this.noOfPlayers();
-			scanner.close();
+			noOfPlayers();
 		}
+		scanner.close();
+	}
+
+	/*
+	 * In this method we have defined the sequence of the player. We are itterating
+	 * the loop till player input.
+	 */
+	public void sequenceOfPlay(int player) {
+		System.out.println("\nSequence of cards are below : ");
+		for (int i = 1; i <= player; i++) {
+			System.out.println("\nPlayer " + i + " Getting card.............");
+			toshuffle(cardsDeck);
+		}
+	}
+
+	/*
+	 * Created a method to shuffle the cards Shuffling the cards by using
+	 * Math.random and storing it in temp variable of ArrayList Again we are
+	 * assigning temp to cardDecck
+	 */
+	public static void toshuffle(ArrayList<String> cardsDeck) {
+		System.out.println("shuffling the cards before Distribution");
+		ArrayList<String> temp = new ArrayList<String>();
+		while (!cardsDeck.isEmpty()) {
+			int loc = (int) (Math.random() * cardsDeck.size());
+			temp.add(cardsDeck.get(loc));
+			cardsDeck.remove(loc);
+		}
+		cardsDeck = temp;
+		toDisplay(cardsDeck);
 	}
 }
